9ee2ddbf43e1c3d72f4b1a8dc2890ec10836c2df
Christian Felix <1christianfelix@gmail.com>
2023-01-18 12:31:22 -0800

    Feature 3 Complete

--- brain_two/todos/models.py
+++ brain_two/todos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TodoList(models.Model):
+    # this is a class, remember what classes look like!
+    name = models.CharField(max_length=150)
+    created_on = models.DateTimeField(auto_now_add=True)
